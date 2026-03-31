e4a795e045d141e9e2f954d99044c2af59923fbb
Your GitHub Name <bajpaianimesh13@gmail.com>
2026-03-31 12:37:01 +0530

    Cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import UserRoutes from "./routes/User.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://fitness-tracker-chi-inky.vercel.app"
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 
